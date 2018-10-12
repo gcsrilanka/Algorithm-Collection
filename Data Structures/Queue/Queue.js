ddbffc3346efafc30bda6ab94fc40913f87fcfbd
Петров Евгений Сергеевич <petrov@tochka.com>
2018-10-12 13:56:58 +0500

    JavaScript implementation of the Queue data structure

---
+++ Data Structures/Queue/Queue.js
@@ -0,0 +1,55 @@
+'use strict';
+
+class Queue {
+    constructor() {
+        this.empty();
+    }
+
+    /**
+     * Adds an item to the end of the queue
+     * @param item
+     */
+    enqueue(item) {
+        this._queue.push(item);
+    }
+
+    /**
+     * Retrieves and returns the first element of the queue
+     * @returns first element of the queue
+     */
+    dequeue() {
+        return this._queue.shift();
+    }
+
+    /**
+     * Returns the first item from the beginning of the queue 
+     * without deleting it
+     * @returns first element of the queue
+     */
+    peek() {
+        return this._queue[0];
+    }
+
+    /**
+     * Clears the queue
+     */
+    empty() {
+        this._queue = [];
+    }
+
+    /**
+     * Check for queue emptiness
+     * @returns true/false
+     */
+    get isEmpty() {
+        return !this._queue.length;
+    }
+
+    /**
+     * Queue length
+     * @returns length
+     */
+    get length() {
+        return this._queue.length;
+    }
+}
