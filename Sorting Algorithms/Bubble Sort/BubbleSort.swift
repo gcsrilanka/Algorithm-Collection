7039cdb43f936dfd8859a7de18da435aa507ed4b
Pubudu Mihiranga De Silva <pubudu.mihiranga@circles.asia>
2021-10-20 00:09:51 +0530

    update bubble sort algorithm logic

--- Sorting Algorithms/Bubble Sort/BubbleSort.swift
+++ Sorting Algorithms/Bubble Sort/BubbleSort.swift
@@ -9,11 +9,16 @@ import Foundation
 
 //MARK: - Bubble Sort algorithm
 
-func bubbleSort(_ dataArray: [Int]) -> [Int] {
-    var unsortedArray = dataArray
+func bubbleSort(_ dataArray: [Int]?) -> [Int] {
     
-    if unsortedArray.count <= 1 {
-        return dataArray
+    //check if dataArray is nil
+    guard var unsortedArray = dataArray else {
+        return []
+    }
+    
+    //check if dataArray length is equal 1
+    if unsortedArray.count == 1 {
+        return dataArray!
     }
     
     let lastPosition = unsortedArray.count - 1
@@ -36,9 +41,18 @@ func bubbleSort(_ dataArray: [Int]) -> [Int] {
 
 //MARK: - Usage
 
-let dataArray = [3, 4, 5, 12, 0, 8, 1]
-print("Unsorted := \(dataArray)") //output := Unsorted := [3, 4, 5, 1, 0, 8, 1]
+let dataArray1 = [3, 4, 5, 12, 0, 8, 1]
+print("Unsorted := \(dataArray1)") //output := Unsorted := [3, 4, 5, 1, 0, 8, 1]
+let sortedArray1 = bubbleSort(dataArray1)
+print("Sorted := \(sortedArray1)") //output := Sorted := [0, 1, 1, 3, 4, 5, 8]
 
-let sortedArray = bubbleSort(dataArray)
-print("Sorted := \(sortedArray)") //output := Sorted := [0, 1, 1, 3, 4, 5, 8]
+let dataArray2: [Int]? = nil
+print("Unsorted := \(dataArray2)") //output := Unsorted := nil
+let sortedArray2 = bubbleSort(dataArray2)
+print("Sorted := \(sortedArray2)") //output := Sorted := []
+
+let dataArray3 = [5]
+print("Unsorted := \(dataArray3)") //output := Unsorted := [5]
+let sortedArray3 = bubbleSort(dataArray3)
+print("Sorted := \(sortedArray3)") //output := Sorted := [5]
 
