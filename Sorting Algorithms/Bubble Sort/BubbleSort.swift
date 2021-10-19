2a0443b3dc90db550abaf2a0a82630502f65572f
Pubudu Mihiranga De Silva <pubudu.mihiranga@circles.asia>
2021-10-19 17:45:12 +0530

    update the bubble sort logic

--- Sorting Algorithms/Bubble Sort/BubbleSort.swift
+++ Sorting Algorithms/Bubble Sort/BubbleSort.swift
@@ -1,6 +1,6 @@
 //
 //  BubbleSort.swift
-//  
+//
 //
 //  Created by Pubudu Mihiranga on 19/10/21.
 //
@@ -11,16 +11,26 @@ import Foundation
 
 func bubbleSort(_ dataArray: [Int]) -> [Int] {
     var unsortedArray = dataArray
-    for _ in 0...unsortedArray.count {
-        for value in 1...unsortedArray.count - 1 {
-            if unsortedArray[value-1] > unsortedArray[value] {
-                let tempValue = unsortedArray[value-1]
-                unsortedArray[value-1] = unsortedArray[value]
-                unsortedArray[value] = tempValue
+    
+    if unsortedArray.count <= 1 {
+        return dataArray
+    }
+    
+    let lastPosition = unsortedArray.count - 1
+    var shouldSwap = true
+    
+    while shouldSwap {
+        shouldSwap = false
+        for i in 1...lastPosition {
+            if unsortedArray[i-1] > unsortedArray[i] {
+                let tempValue = unsortedArray[i-1]
+                unsortedArray[i-1] = unsortedArray[i]
+                unsortedArray[i] = tempValue
+                shouldSwap = true
             }
         }
     }
-    
+
     return unsortedArray
 }
 
@@ -31,3 +41,4 @@ print("Unsorted := \(dataArray)") //output := Unsorted := [3, 4, 5, 1, 0, 8, 1]
 
 let sortedArray = bubbleSort(dataArray)
 print("Sorted := \(sortedArray)") //output := Sorted := [0, 1, 1, 3, 4, 5, 8]
+
