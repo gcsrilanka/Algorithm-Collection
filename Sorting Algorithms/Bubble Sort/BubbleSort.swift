ec275f4df58b8d3f3460eacd326fc3b6db90e996
Pubudu Mihiranga De Silva <pubudu.mihiranga@circles.asia>
2021-10-20 00:32:38 +0530

    update bubble sort algorithm logic

--- Sorting Algorithms/Bubble Sort/BubbleSort.swift
+++ Sorting Algorithms/Bubble Sort/BubbleSort.swift
@@ -17,7 +17,7 @@ func bubbleSort(_ dataArray: [Int]?) -> [Int] {
     }
     
     //check if dataArray length is equal 1
-    if unsortedArray.count == 1 {
+    if unsortedArray.count < 2 {
         return unsortedArray
     }
     
